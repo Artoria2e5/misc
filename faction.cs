16ea6482eda095bf39bafec94a17de947043a8a7
Arthur200000 <arthur200126@gmail.com>
2014-05-14 15:00:56 +0800

    faked apt-spy
    @TheNextProject look at that..

---
+++ faction.cs
@@ -0,0 +1,16 @@
+//Poor implentation
+using System;
+namespace faction {
+  class faction {
+    public struct output {
+      int am;
+      int bm;
+    }
+    int m
+    int n
+    string[] input = System.IO.File.ReadAllLines(@"C:\Users\Public\TestFolder\WriteText.txt");
+    int[] p= new int[10];
+  }
+}
+
+
