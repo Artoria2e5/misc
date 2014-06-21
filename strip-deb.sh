9238d5e25d1b72e5c1f3d0af4a263c7fcabf8e54
Arthur Wang <arthur200126@gmail.com>
2014-06-21 10:30:56 +0800

    Update strip-deb.sh

--- strip-deb.sh
+++ strip-deb.sh
@@ -12,7 +12,7 @@ elfstrip(){
 	( file $1 | grep '\: ELF' > /dev/null )	&& (( echo "$i" | grep "/bin/" 1>/dev/null 2>&1 || echo "$i" | grep "/sbin" 1>/dev/null 2>&1 ) && strip --strip-all $1 2> /dev/null || strip --strip-debug $1 2> /dev/null ) || return 0 
 }
 fltr_elffltr(){
-        for i in usr/lib lib bin usr/bin opt/*/lib opt/*/bin
+        for i in usr/lib lib bin usr/bin opt/*/lib opt/*/bin sbin usr/sbin
         do
                 [ -d $i ] || continue
                 pushd $i >/dev/null
