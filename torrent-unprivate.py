1f7559ea5c9f5580d2dc4d9b5d8333f4f6879a7a
Mingye Wang <arthur200126@gmail.com>
2020-08-21 03:55:52 +0800

    Update torrent-unprivate.py

--- torrent-unprivate.py
+++ torrent-unprivate.py
@@ -2,7 +2,10 @@
 """
 Make a given torrent file non-private.
 
+```bash
 ./torrent-unprivate.py *.torrent
+mv *.pub *.pub.torrent my-public-cave/
+```
 """
 import sys
 import os
@@ -10,22 +13,19 @@ from torrentool.api import Torrent
 from typing import Optional
 
 
-def unpriv(path: str, link: Optional[bool]) -> None:
-    if link is None:
-        link = "try"
-
+def unpriv(path: str, link: Optional[bool] = None) -> None:
     t = Torrent.from_file(path)
     t.private = False
-    d: str, _ = os.path.splitext(path)
+    d, _ = os.path.splitext(path)
     fp = d + ".pub.torrent"
     t.to_file(fp)
     print("{}\t{}".format(t.magnet_link, fp))
 
-    if os.path.isdir(d) and link:
+    if os.path.isdir(d) and link is not False:
         bd = os.path.basename(d)
         try:
             os.symlink(bd, bd + ".pub")
-        except e:
+        except BaseException as e:
             if link is True:
                 raise
 
