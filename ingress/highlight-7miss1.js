b826786dbefe581ed6eb41eb273b8d067b1ea04b
Mingye Wang <arthur200126@gmail.com>
2024-06-01 11:08:46 +0800

    i/h7m1: Generate correct dash count for L5, L6.

--- ingress/highlight-7miss1.js
+++ ingress/highlight-7miss1.js
@@ -16,153 +16,145 @@
 /* exported setup, changelog --eslint */
 /* global L */
 function wrapper(plugin_info) {
-  console.log("wrapper");
-  // ensure plugin framework is there, even if iitc is not yet loaded
-  if (typeof window.plugin !== "function") window.plugin = function () {};
+	// ensure plugin framework is there, even if iitc is not yet loaded
+	if (typeof window.plugin !== "function") window.plugin = function () {};
 
-  //PLUGIN AUTHORS: writing a plugin outside of the IITC build environment? if so, delete these lines!!
-  //(leaving them in place might break the 'About IITC' page or break update checks)
-  /*
-  plugin_info.buildName = 'local';
-  plugin_info.dateTimeVersion = '2024-04-04-045100';
-  plugin_info.pluginId = 'highlight-7miss1';
-  */
-  //END PLUGIN AUTHORS NOTE
+	//PLUGIN AUTHORS: writing a plugin outside of the IITC build environment? if so, delete these lines!!
+	//(leaving them in place might break the 'About IITC' page or break update checks)
+	/*
+	plugin_info.buildName = 'local';
+	plugin_info.dateTimeVersion = '2024-04-04-045100';
+	plugin_info.pluginId = 'highlight-7miss1';
+	*/
+	//END PLUGIN AUTHORS NOTE
 
-  const changelog = [
-    {
-      version: "0.0.1",
-      changes: ["New"],
-    },
-    {
-      version: "0.0.2",
-      changes: [
-        "Always use cache regardless of freshness -- should reduce request count.",
-      ],
-    },
-    {
-      version: "0.0.3",
-      changes: [
-        "Exclude self deployed portals.",
-        "Exclude Machina from requests.",
-        "Include portals with level equal to resCount.",
-        "Change color scheme (SunsetDark).",
-        "Remove debug prints.",
-      ],
-    },
-    {
-      version: "0.0.4",
-      changes: [
-        "Add dashed stroke for the colorblind.",
-        "Change color scheme again (Plasma).",
-        "Fill with grey and dash when own L8 reso present.",
-        "Do not exclude own non-L8 reso.",
-      ],
-    },
-    {
-      version: "0.0.5",
-      changes: [
-        "Change color scheme again to maximize chroma.", 
-      ],
-    }
-  ];
+	const changelog = [
+		{
+			version: "0.0.1",
+			changes: ["New"],
+		},
+		{
+			version: "0.0.2",
+			changes: ["Always use cache regardless of freshness -- should reduce request count."],
+		},
+		{
+			version: "0.0.3",
+			changes: [
+				"Exclude self deployed portals.",
+				"Exclude Machina from requests.",
+				"Include portals with level equal to resCount.",
+				"Change color scheme (SunsetDark).",
+				"Remove debug prints.",
+			],
+		},
+		{
+			version: "0.0.4",
+			changes: [
+				"Add dashed stroke for the colorblind.",
+				"Change color scheme again (Plasma).",
+				"Fill with grey and dash when own L8 reso present.",
+				"Do not exclude own non-L8 reso.",
+			],
+		},
+		{
+			version: "0.0.5",
+			changes: [
+				"Tweak color scheme again to maximize chroma.",
+				"Generate correct dash count for L5, L6."
+			],
+		},
+	];
 
-  // use own namespace for plugin
-  const self = {};
-  window.plugin.highlightSevenMissOne = self;
+	// use own namespace for plugin
+	const self = {};
+	window.plugin.highlightSevenMissOne = self;
 
-  // chosen by python-colorspace `sequential_hcl("Plasma").colors(7)[1::2]`
-  self.styles = {
-    common: {
-      fillOpacity: 0.85,
-      linecap: "butt",
-    },
-    // Modified by: setting oklch L to 45, then maximize chroma
-    // chroma-maximization code: https://gist.github.com/Artoria2e5/9c7ba0bcda480b5bc2ae0b0ffe0bfb91
-    sev_miss_1: { fillColor: "#8400a1" },
-    // chroma-maximized too
-    sev_miss_2: { fillColor: "#ef0069" },
-    sev_miss_3: { fillColor: "#ECC000" },
-    hasOwn: { fillColor: "#808080" },
-  };
+	// chosen by python-colorspace `sequential_hcl("Plasma").colors(7)[1::2]`
+	self.styles = {
+		common: {
+			fillOpacity: 0.85,
+			linecap: "butt",
+		},
+		// Modified by: setting oklch L to 45, then maximize chroma
+		// chroma-maximization code: https://gist.github.com/Artoria2e5/9c7ba0bcda480b5bc2ae0b0ffe0bfb91
+		sev_miss_1: { fillColor: "#8400a1" },
+		// chroma-maximized too
+		sev_miss_2: { fillColor: "#ef0069" },
+		sev_miss_3: { fillColor: "#ECC000" },
+		hasOwn: { fillColor: "#808080" },
+	};
 
-  self.dashArrayMemo = {
-    scale: -1,
-  };
+	self.dashArrayMemo = {
+		scale: -1,
+	};
 
-  self.makeDashArray = function (scale, dashes) {
-    if (self.dashArrayMemo.scale === scale && dashes in self.dashArrayMemo) {
-      return self.dashArrayMemo[dashes];
-    }
-    self.dashArrayMemo.scale = scale;
-    // LEVEL_TO_RADIUS[7] === 10; 2pi ~= 6.3
-    const c = 63 * scale;
-    const unit = c / dashes / 3;
-    const da = (self.dashArrayMemo[dashes] = `${unit}, ${unit * 2}`);
-    return da;
-  };
+	self.makeDashArray = (scale, dashes, level = 7) => {
+		const cacheKey = level < 7 ? -dashes : dashes;
+		if (self.dashArrayMemo.scale === scale && cacheKey in self.dashArrayMemo) {
+			return self.dashArrayMemo[cacheKey];
+		}
 
-  self.checkDetail = function (data, details) {
-    // console.log("candidate " + data.portal.options.guid)
-    if (details === undefined) {
-      // console.log("no details ")
-      return;
-    }
-    const reso = details.resonators;
+		self.dashArrayMemo.scale = scale;
+		const LEVEL_TO_RADIUS = [7, 7, 7, 7, 8, 8, 9, 10, 11];
+		// 2pi ~= 6.3
+		const circ = LEVEL_TO_RADIUS[level] * 6.3 * scale;
+		const unit = circ / dashes / 3;
+		const da = (self.dashArrayMemo[cacheKey] = `${unit}, ${unit * 2}`);
+		return da;
+	};
 
-    const reso8 = reso.filter((x) => x.level === 8);
-    const reso_sum = reso8.length;
-    const has_own = reso8.some((x) => x.owner === PLAYER.nickname);
-    const reso_needed = 8 - reso_sum;
-    // console.log("reso_needed " + reso_needed)
+	self.checkDetail = (data, details) => {
+		if (details === undefined) {
+			return;
+		}
+		const reso = details.resonators;
 
-    if (reso_needed == 0 || reso_needed > 3) return;
+		const reso8 = reso.filter((x) => x.level === 8);
+		const reso_sum = reso8.length;
+		const has_own = reso8.some((x) => x.owner === PLAYER.nickname);
+		const reso_needed = 8 - reso_sum;
 
-    data.portal.setStyle(
-      L.extend(
-        {},
-        self.styles.common,
-        has_own ? self.styles.hasOwn : self.styles["sev_miss_" + reso_needed],
-        { dashArray: self.makeDashArray(window.portalMarkerScale(), reso_sum) },
-      ),
-    );
-  };
+		if (reso_needed == 0 || reso_needed > 3) return;
 
-  self.highlight = function (data) {
-    const portal_data = data.portal.options.data;
-    const portal_level = portal_data.level;
-    if (
-      !(
-        portal_level === 7 ||
-        (portal_level >= 5 && portal_level === portal_data.resCount)
-      )
-    )
-      return;
-    if (portal_data.team === "M") return;
-    const guid = data.portal.options.guid;
+		data.portal.setStyle(
+			L.extend(
+				{},
+				self.styles.common,
+				has_own ? self.styles.hasOwn : self.styles["sev_miss_" + reso_needed],
+				{ dashArray: self.makeDashArray(window.portalMarkerScale(), reso_sum, details.level) }
+			)
+		);
+	};
 
-    // Accept old data (false). Only request when completely missing (undefined).
-    if (window.portalDetail.isFresh(guid) === undefined) {
-      const req_promise = window.portalDetail.request(guid);
-      req_promise.then(function (_) {
-        self.checkDetail(data, window.portalDetail.get(guid));
-      });
-    }
-    self.checkDetail(data, window.portalDetail.get(guid));
-  };
+	self.highlight = (data) => {
+		const portal_data = data.portal.options.data;
+		const portal_level = portal_data.level;
+		if (!(portal_level === 7 || (portal_level >= 5 && portal_level === portal_data.resCount)))
+			return;
+		if (portal_data.team === "M") return;
+		const guid = data.portal.options.guid;
 
-  setup = self.setup = function () {
-    window.addPortalHighlighter("Almost-8 Portals", self.highlight);
-  };
+		// Accept old data (false). Only request when completely missing (undefined).
+		if (window.portalDetail.isFresh(guid) === undefined) {
+			const req_promise = window.portalDetail.request(guid);
+			req_promise.then(function (_) {
+				self.checkDetail(data, window.portalDetail.get(guid));
+			});
+		}
+		self.checkDetail(data, window.portalDetail.get(guid));
+	};
 
-  setup.info = plugin_info; //add the script info data to the function as a property
-  if (typeof changelog !== "undefined") setup.info.changelog = changelog;
-  if (!window.bootPlugins) window.bootPlugins = [];
-  window.bootPlugins.push(setup);
-  // if IITC has already booted, immediately run the 'setup' function
-  if (window.iitcLoaded && typeof setup === "function") setup();
+	setup = self.setup = () => {
+		window.addPortalHighlighter("Almost-8 Portals", self.highlight);
+	};
+
+	setup.info = plugin_info; //add the script info data to the function as a property
+	if (typeof changelog !== "undefined") setup.info.changelog = changelog;
+	if (!window.bootPlugins) window.bootPlugins = [];
+	window.bootPlugins.push(setup);
+	// if IITC has already booted, immediately run the 'setup' function
+	if (window.iitcLoaded && typeof setup === "function") setup();
 } // wrapper end
-
 
 // inject code into site context
 const script = document.createElement('script');
