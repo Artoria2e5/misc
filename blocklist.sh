f8c3c92741dc35469cd4104a1ebbbfc60c3f0aab
Mingye Wang <arthur200126@gmail.com>
2024-08-27 14:24:22 +0800

    Update blocklist.sh

--- blocklist.sh
+++ blocklist.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -x
 # 0 21 * * * sleep $((RANDOM%3600)) && /opt/blocklist/blocklist.sh
+PATH=/bin:/usr/bin:/sbin:/usr/sbin:/usr/local/bin:/usr/local/sbin
 # https://sysadmin.info.pl/en/blog/using-blocklist-with-iptables-and-firewalld/
+# dep https://github.com/zhanhb/cidr-merger
 IP_BLOCKLIST=/etc/ip-blocklist.conf
 IP_BLOCKLIST_TMP=/tmp/ip-blocklist.tmp
 IP_BLOCKLIST_CUSTOM=/etc/ip-blocklist-custom.conf # optional
@@ -40,33 +42,33 @@ r() {
 
 add_rule "$IP_BLOCKLIST_CUSTOM"
 
-r "http://www.projecthoneypot.org/list_of_ips.php?t=d&rss=1"           # Project Honey Pot Directory of Dictionary Attacker IPs
-r "http://check.torproject.org/cgi-bin/TorBulkExitList.py?ip=1.1.1.1"  # TOR Exit Nodes (ipsum)
+# "https://www.projecthoneypot.org/list_of_ips.php?t=d&rss=1"          # Project Honey Pot Directory of Dictionary Attacker IPs
+# "https://check.torproject.org/cgi-bin/torbulkexitlist?ip=1.1.1.1"    # TOR Exit Nodes (ipsum)
 # "http://www.maxmind.com/en/anonymous_proxies" 30                     # MaxMind GeoIP Anonymous Proxies
 # "https://www.maxmind.com/en/high-risk-ip-sample-list" 30             # MaxMind High Risk Sample List
-r "http://danger.rulez.sk/projects/bruteforceblocker/blist.php"        # BruteForceBlocker IP List
+# "https://danger.rulez.sk/projects/bruteforceblocker/blist.php"       # BruteForceBlocker IP List
 # "https://rules.emergingthreats.net/blockrules/compromised-ips.txt"   # Emerging Threats - Russian Business Networks List
 # "http://www.spamhaus.org/drop/drop.lasso"                            # Spamhaus Don't Route Or Peer List (DROP)
 r "http://cinsscore.com/list/ci-badguys.txt"                           # C.I. Army Malicious IP List
 # "http://www.autoshun.org/files/shunlist.csv"                         # Autoshun Shun List
-r "http://lists.blocklist.de/lists/all.txt" 1                          # blocklist.de fail2ban reporting service
-r "https://fx.vc-mp.eu/shared/iplist.txt"                              # ferex badlist
-r "https://feodotracker.abuse.ch/downloads/ipblocklist_aggressive.txt" # FEODO tracker
-r "https://reputation.alienvault.com/reputation.generic"               # ALIENVAULT REPUTATION
-r "http://www.darklist.de/raw.php"                                     # DARKLIST DE
-# "http://osint.bambenekconsulting.com/feeds/c2-dommasterlist-high.txt" 1
-# "http://osint.bambenekconsulting.com/feeds/c2-dommasterlist.txt" 1
-r "http://osint.bambenekconsulting.com/feeds/c2-ipmasterlist-high.txt" 1
-r "http://osint.bambenekconsulting.com/feeds/c2-ipmasterlist.txt" 1
-# "http://osint.bambenekconsulting.com/feeds/c2-masterlist.txt" 1
-# "http://osint.bambenekconsulting.com/feeds/dga-feed.txt" 1
+r "https://lists.blocklist.de/lists/all.txt" 1                         # blocklist.de fail2ban reporting service
+# "https://fx.vc-mp.eu/shared/iplist.txt"                              # ferex badlist
+# "https://feodotracker.abuse.ch/downloads/ipblocklist.txt"            # FEODO tracker
+# "https://reputation.alienvault.com/reputation.generic"               # ALIENVAULT REPUTATION
+# "http://www.darklist.de/raw.php"                                     # DARKLIST DE
+# "https://osint.bambenekconsulting.com/feeds/c2-dommasterlist-high.txt" 1
+# "https://osint.bambenekconsulting.com/feeds/c2-dommasterlist.txt" 1
+# "https://osint.bambenekconsulting.com/feeds/c2-ipmasterlist-high.txt" 1
+# "https://osint.bambenekconsulting.com/feeds/c2-ipmasterlist.txt" 1
+# "https://osint.bambenekconsulting.com/feeds/c2-masterlist.txt" 1
+# "https://osint.bambenekconsulting.com/feeds/dga-feed.txt" 1
 r "https://www.binarydefense.com/banlist.txt"                            # Binary Defense Systems
-r "https://raw.githubusercontent.com/stamparm/ipsum/master/levels/2.txt" # https://github.com/stamparm/ipsum
-r "http://sblam.com/blacklist.txt"                                       # SBLAM
-r "http://blocklist.greensnow.co/greensnow.txt"
-r "http://charles.the-haleys.org/ssh_dico_attack_hdeny_format.php/hostsdeny.txt"
+r "https://raw.githubusercontent.com/stamparm/ipsum/master/levels/2.txt" 1 # https://github.com/stamparm/ipsum
+# "https://sblam.com/blacklist.txt"                                      # SBLAM
+# "http://blocklist.greensnow.co/greensnow.txt"
+# "https://charles.the-haleys.org/ssh_dico_attack_hdeny_format.php/hostsdeny.txt"
 # "https://www.malwaredomainlist.com/hostslist/ip.txt"
-r "https://www.stopforumspam.com/downloads/toxic_ip_cidr.txt"
+# "https://www.stopforumspam.com/downloads/toxic_ip_cidr.txt" 30
 # 'http://list.iblocklist.com/?list=erqajhwrxiuvjxqrrwfj&fileformat=p2p&archiveformat=gz'
 
 #Consolidate the shodan.io IP addresses database
@@ -75,6 +77,10 @@ r "https://www.stopforumspam.com/downloads/toxic_ip_cidr.txt"
 #Sort the list
 cidr-merger <$IP_BLOCKLIST_TMP >$IP_BLOCKLIST
 cidr-merger <$IP_BLOCKLIST_TMP.6 >$IP_BLOCKLIST.6
+
+grep -v '^117\.' $IP_BLOCKLIST > $IP_BLOCKLIST.tmp
+mv $IP_BLOCKLIST{.tmp,}
+
 
 #Remove temporary list
 rm $IP_BLOCKLIST_TMP
@@ -97,8 +103,8 @@ nextpow2() {
   ))
 }
 ((
-  BLSIZE = BLSIZE * 4 / 3,
-  BLSIZE6 = BLSIZE6 * 4 / 3
+  BLSIZE = BLSIZE * 2,
+  BLSIZE6 = BLSIZE6 * 2
 ))
 nextpow2 BLSIZE
 nextpow2 BLSIZE6
@@ -109,42 +115,44 @@ mkdir -p /etc/nftables/idempotent
 # Generate nftables.
 {
   cat <<EOF
-add table ip bl
-add set ip bl blocklist { type ipv4_addr; size $BLSIZE; flags interval; }
-flush set bl blocklist
+add table ip bl4
+add set ip bl4 blocklist { type ipv4_addr; size $BLSIZE; flags interval; }
+flush set bl4 blocklist
+add element ip bl4 blocklist {
 EOF
-  while IFS= read -r line; do
-    printf 'add element ip bl blocklist { %s }\n' "$line"
-  done </etc/ip-blocklist.conf
+  printf '\t'
+  perl -p -e 's/\n/,\n\t/g' <$IP_BLOCKLIST
   cat <<EOF
-destroy chain ip bl input
-destroy chain ip bl forward
-add chain ip bl input { type filter hook input priority 0; }
-add chain ip bl forward { type filter hook forward priority 0; }
-add rule ip bl input ip saddr @blocklist drop
-add rule ip bl forward ip saddr @blocklist drop
+}
+destroy chain ip bl4 input
+destroy chain ip bl4 forward
+add chain ip bl4 input { type filter hook input priority 0; }
+add chain ip bl4 forward { type filter hook forward priority 0; }
+add rule ip bl4 input ip saddr @blocklist log level audit drop
+add rule ip bl4 forward ip saddr @blocklist log level audit drop
 EOF
-} > /etc/nftables/idempotent/ip-blocklist.nft
+} > /etc/nftables/idempotent/ipblocklist.nft
 
 {
   cat <<EOF
 add table ip6 bl6
-add set ip6 bl6 blocklist6 { type ipv6_addr; size $((BLSIZE6 * 2)); flags interval; }
-flush set ip6 bl6 blocklist6
+add set ip6 bl6 blocklist { type ipv6_addr; size $BLSIZE6; flags interval; }
+flush set ip6 bl6 blocklist
+add element ip6 bl6 blocklist {
 EOF
-  while IFS= read -r line; do
-    printf 'add element ip6 bl6 blocklist6 { %s }\n' "$line"
-  done </etc/ip-blocklist.conf.6
+  printf '  '
+  perl -p -e 's/\n/, /g' <$IP_BLOCKLIST.6
   cat <<EOF
+}
 destroy chain ip6 bl6 input
-destroy chain ip6 bl6 forward 
+destroy chain ip6 bl6 forward
 add chain ip6 bl6 input { type filter hook input priority 0; }
 add chain ip6 bl6 forward { type filter hook forward priority 0; }
-add rule ip6 bl6 input ip6 saddr @blocklist6 drop
-add rule ip6 bl6 forward ip6 saddr @blocklist6 drop
+add rule ip6 bl6 input ip6 saddr @blocklist log level audit drop
+add rule ip6 bl6 forward ip6 saddr @blocklist log level audit drop
 EOF
-} > /etc/nftables/idempotent/ip-blocklist6.nft
+} > /etc/nftables/idempotent/ipblocklist6.nft
 
 # Reload nftables.
-nft -o -f /etc/nftables/idempotent/ip-blocklist.nft
-nft -o -f /etc/nftables/idempotent/ip-blocklist6.nft
+nft -f /etc/nftables/idempotent/ipblocklist.nft
+nft -f /etc/nftables/idempotent/ipblocklist6.nft
