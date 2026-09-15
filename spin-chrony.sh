f0f07d65d94150880a0a478a27b8bbf409cdca67
Mingye Wang <arthur200126@gmail.com>
2026-09-15 23:21:16 +0800

    Update hashlimit expire time for NTP packets
    Increased hashlimit expiration time for NTP traffic.

--- spin-chrony.sh
+++ spin-chrony.sh
@@ -142,7 +142,7 @@ IPTABLES='*raw
 :OUTPUT ACCEPT [0:0]
 -A PREROUTING -p udp -m udp --dport 123 -j NOTRACK
 -A PREROUTING -p udp -m udp --dport 11123 -j NOTRACK
--A PREROUTING -p udp -m udp --dport 123 -m hashlimit --hashlimit-above 1/sec --hashlimit-burst 16 --hashlimit-mode srcip --hashlimit-name ntp1 --hashlimit-htable-max 4096 --hashlimit-htable-expire 8000 --hashlimit-srcmask @srcmark@ -j DROP
+-A PREROUTING -p udp -m udp --dport 123 -m hashlimit --hashlimit-above 1/sec --hashlimit-burst 16 --hashlimit-mode srcip --hashlimit-name ntp1 --hashlimit-htable-max 4096 --hashlimit-htable-expire 30000 --hashlimit-srcmask @srcmark@ -j DROP
 -A PREROUTING -p udp -m udp --dport 123 -m statistic --mode random --probability 0.06250000000 -j ACCEPT
 -A PREROUTING -p udp -m udp --dport 123 -m hashlimit --hashlimit-above 1/min --hashlimit-burst 8 --hashlimit-mode srcip --hashlimit-name ntp --hashlimit-htable-max 131072 --hashlimit-htable-expire 600000 --hashlimit-srcmask @srcmark@ -j DROP
 -A OUTPUT -p udp -m udp --sport 123 -j NOTRACK
